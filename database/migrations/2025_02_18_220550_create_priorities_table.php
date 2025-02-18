543b6f50ff2270220ab50dba80e47438e7acd54a
VitorRocha <vitormvr2001@gmail.com>
2025-02-18 22:17:36 +0000

    changes

--- database/migrations/2025_02_18_220550_create_priorities_table.php
+++ database/migrations/2025_02_18_220550_create_priorities_table.php
@@ -4,19 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('priorities', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->enum('status', ['active','inactive'])->default('active');
-            $table->timestamps();
-        });
+        Schema::create(
+            'priorities',
+            function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->enum('status', ['active','inactive'])->default('active');
+                $table->timestamps();
+            }
+        );
     }
 
     /**
